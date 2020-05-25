43b4f1154711a491172af98eda1034e33fa32b2a
Debian <admin@ip-172-31-4-80.ap-south-1.compute.internal>
2020-05-25 13:18:39 +0000

    master version change

--- react/features/toolbox/components/web/Toolbox.js
+++ react/features/toolbox/components/web/Toolbox.js
@@ -337,7 +337,7 @@ class Toolbox extends Component<Props, State> {
                     }
                     else if(APP.conference._room.isAdmin == false || APP.conference._room.isAdmin == "false")
                     {
-                        this.setState({show:true});
+                        this.setState({show:false});
                     }
                     else
                     {
