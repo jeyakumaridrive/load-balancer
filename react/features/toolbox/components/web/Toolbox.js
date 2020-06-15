0bea442efc5b5b7c5c04c4ee55a48386df2e869a
Naveen Raghuvanshi <naveen@olecons.com>
2020-06-16 02:44:57 +0530

    change url after meeting loads

--- react/features/toolbox/components/web/Toolbox.js
+++ react/features/toolbox/components/web/Toolbox.js
@@ -1197,6 +1197,7 @@ class Toolbox extends Component<Props, State> {
             if(!resolve) {
                 location.href="https://meeting.remotepc.com/home/";
             }
+            window.history.pushState({}, document.title, 'https://meeting.remotepc.com/meet/'+resolve.slug);
             $('.cw_meeting-url').text(parentDomain+'/meet/'+resolve.slug);
             if(APP.password) {
                 $('.cw_meeting-password').show();
