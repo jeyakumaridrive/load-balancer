8a85244dc5e16abcacfcae96beeef91303589f49
root <root@ip-172-31-4-80.ap-south-1.compute.internal>
2020-06-19 10:44:48 +0000

    safari css bug fixes

--- react/features/device-selection/components/DeviceSelection.js
+++ react/features/device-selection/components/DeviceSelection.js
@@ -164,6 +164,39 @@ class DeviceSelection extends AbstractDialogTab<Props, State> {
         ])
         .catch(err => logger.warn('Failed to initialize preview tracks', err))
         .then(() => this.props.mountCallback && this.props.mountCallback());
+
+
+
+        console.log("Adding Animation");
+        var stream = APP.conference.localAudio.stream;
+        var ctx = new (window.AudioContext || window.webkitAudioContext)();
+        var audioSrc = ctx.createMediaStreamSource(stream);
+        var analyser = ctx.createAnalyser();
+        window.scriptProcessor = ctx.createScriptProcessor();
+        audioSrc.connect(analyser);
+        analyser.connect(scriptProcessor);
+        scriptProcessor.connect(ctx.destination);
+        analyser.fftSize = 32;
+        var ot = 0;
+        window.scriptProcessor.onaudioprocess = function (e) {
+            var tempArray = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(tempArray);
+            var t = 0;
+            for (let i = 0; i < tempArray.length; i++) {
+                t += tempArray[i] / (256 * 16);
+            }
+            // console.log(ot, t);
+            if (t > 0.15 || t > ot + 0.5) {
+                // console.log('animating');
+                $(".audio-preview-container .audio-wave").addClass('animate')
+                ot = t;
+            } else if (t < 0.15 || t < ot - 0.5) {
+                // console.log('not animating');
+                $(".audio-preview-container .audio-wave").removeClass('animate')
+                ot = t;
+            }
+        };
+
     }
 
     /**
@@ -241,13 +274,23 @@ class DeviceSelection extends AbstractDialogTab<Props, State> {
                                 error = { this.state.previewVideoTrackError }
                                 track = { this.state.previewVideoTrack } />
                          </div>
-                    { !hideAudioInputPreview
-                        && <AudioInputPreview
-                            track = { this.state.previewAudioTrack } /> }
 
+
+                        
+                         { hideAudioInputPreview &&
+                            <div className="audio-preview-container">
+                                <Icon src={IconMicrophone} />
+                                <div className="audio-wave">
+                                    <span />
+                                    <span />
+                                    <span />
+                                </div>
+                            </div>
+                        }
                     { !hideAudioOutputSelect
                         && <AudioOutputPreview
                             deviceId = { selectedAudioOutputId } /> }
+
                 </div>
             </div>
         );
