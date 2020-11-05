043c6f843a98e56c0da7b3d2890e80243788ae62
Naveen Raghuvanshi <naveen@olecons.com>
2020-11-05 18:51:26 +0530

    jitsi-upgrade update dependency internal

--- react/features/conference/components/web/Subject.js
+++ react/features/conference/components/web/Subject.js
@@ -49,7 +49,7 @@ class Subject extends Component<Props> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            galleryView: !APP.store.getState()['features/video-layout'].tileViewEnabled
+            galleryView: APP.store.getState()['features/video-layout'].tileViewEnabled
         };
         var _this = this;
         setInterval(()=>{
